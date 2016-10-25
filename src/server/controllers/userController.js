39fe09cf9cd85ad89af6c49d83ae9b84eabe6a53
David Wilson <w.dave.w@gmail.com>
2016-10-25 15:29:21 -0700

    Update elo in database on click

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -21,11 +21,10 @@ function getNewElo(req, res) {
   const elo = new Elo();
   const user1Promise = User.findOne({ where: { username: req.params.username1 } }).then(data => { user1 = data; })
   const user2Promise = User.findOne({ where: { username: req.params.username2 } }).then(data => { user2 = data; })
-  Promise.all([user1Promise, user2Promise]).then((fullfill, reject) => {
-      const newUser1Elo = elo.newRatingIfWon(user1.elo, user2.elo);
-      const newUser2Elo = elo.newRatingIfLost(user2.elo, user1.elo);
-      console.log({ yourNewElo: newUser1Elo, theirNewElo: newUser2Elo })
-      res.json({ yourNewElo: newUser1Elo, theirNewElo: newUser2Elo });
+  Promise.all([user1Promise, user2Promise]).then(() => {
+      user1.update({'elo': elo.newRatingIfWon(user1.elo, user2.elo)})
+      .then(user2.update({'elo': elo.newRatingIfLost(user2.elo, user1.elo)}))
+      .then(res.json('updated elo in postgres'));
     })
 }
 
