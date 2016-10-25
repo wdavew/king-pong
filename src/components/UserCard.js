13bf6584be6d203dee84e97e2b6325a755b89b88
David Wilson <w.dave.w@gmail.com>
2016-10-25 15:35:15 -0700

    add win button to user cards

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import ClickableButton from './ClickableButton.js';
 
-function UserCard({id, username, elo, games, league, img}) {
+function UserCard({id, username, elo, games, league, img, handleWinClick}) {
   return (
     <div className='user-card'>
       <p> {username} </p>
       <p> Elo: {elo} </p>
+      <ClickableButton handleClick={() => handleWinClick("Ned_Stark", username)} displayText={'I just beat this person'} />
+
     </div>
   )
 }
